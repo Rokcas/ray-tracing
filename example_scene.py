0bbe99061718b787268ff890533ecc1649740feb
Rokcas <rokas0090@gmail.com>
2023-01-04 20:24:10 +0200

    WIP plane implementation

--- example_scene.py
+++ example_scene.py
@@ -3,6 +3,7 @@ from src.shapes.sphere import Sphere
 from src.shapes.checkered_sphere import CheckeredSphere
 from src.shapes.texture_sphere import TextureSphere
 from src.scene import Screen, Scene, LightSource
+from src.shapes.plane import Plane
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -22,12 +23,15 @@ scene = Scene(
     ),
     [
         TextureSphere(Rgb(0, 0, 1), 0, Vec3(.75, .1, 1), .6, "cat.webp"),
-        Sphere(Rgb(.5, .223, .5), 0.3, Vec3(-.75, .1, 2.25), .6),
-        Sphere(Rgb(1, .572, .184), 0.3, Vec3(-2.75, .1, 3.5), .6),
-        CheckeredSphere(Rgb(.75, .75, .75), 0.25, Vec3(0,-99999.5, 0), 99999),
+        Sphere(Rgb(.5, .223, .5), 0.0, Vec3(-.75, .1, 2.25), .6),
+        Sphere(Rgb(1, .572, .184), 0.0, Vec3(-2.75, .1, 3.5), .6),
+        # CheckeredSphere(Rgb(.75, .75, .75), 0.25, Vec3(0,-99999.5, 0), 99999),
+        Plane(Rgb(.5, .5, .5), 0.0, Vec3(0, 1, 0), Vec3(0, -0.5, 0)),
+        Plane(Rgb(.2, .9, .9), 0, Vec3(0, 0, -1), Vec3(0, 0, 50))
     ],
     [
         LightSource(Vec3(5, 5, -10), Rgb(0.8, 1, 1)),
-        LightSource(Vec3(-10, 10, 0), Rgb(1, 0.9, 0.9))
+        LightSource(Vec3(-10, 10, 0), Rgb(1, 0.9, 0.9)),
+        # LightSource(Vec3(5, 5, 45), Rgb(0.8, 1, 1))
     ]
 )
--- src/shapes/plane.py
+++ src/shapes/plane.py
@@ -3,20 +3,20 @@ import numpy as np
 from src.constants import FARAWAY
 from src.vec3 import Vec3, Rgb
 from dataclasses import dataclass
-from abc import ABC, abstractmethod
+from src.shapes.base import BaseShape
 
 @dataclass
-class BaseShape(ABC):
-    diffuse_colour: Rgb
-    mirror: float
+class Plane(BaseShape):
+    normal: Vec3
+    point: Vec3  # Any point on the plane
 
-    @abstractmethod
     def intersect(self, O, D):
-        pass
+        N = self.normal
+        distance = N.dot(self.point - O) / N.dot(D)
+        return np.where(distance < 0, FARAWAY, distance)
 
-    @abstractmethod
     def normalAt(self, M):
-        pass
+        return self.normal
 
     def diffuseColourAt(self, M):
         return self.diffuse_colour
